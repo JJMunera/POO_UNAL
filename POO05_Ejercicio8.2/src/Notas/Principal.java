1385ffd4564539465f245c4f73ccc54b5d8d5ae6
JJMunera <143112843+JJMunera@users.noreply.github.com>
2023-11-18 14:41:26 -0500

    Add files via upload

---
+++ POO05_Ejercicio8.2/src/Notas/Principal.java
@@ -0,0 +1,10 @@
+package Notas;
+
+public class Principal {
+
+    public static void main(String[]args){
+        Ventana_principal miVentanaPrincipal;
+        miVentanaPrincipal = new Ventana_principal();
+        miVentanaPrincipal.setVisible(true);
+    }    
+}
