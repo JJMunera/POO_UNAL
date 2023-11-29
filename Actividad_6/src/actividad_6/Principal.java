2a57aa93d9367a68bc27719a25d624aacb3b56f6
JJMunera <143112843+JJMunera@users.noreply.github.com>
2023-11-29 14:36:50 -0500

    Add files via upload

---
+++ Actividad_6/src/actividad_6/Principal.java
@@ -0,0 +1,13 @@
+package actividad_6;
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+        Interfaz_grafica formulario= new Interfaz_grafica();
+        formulario.setVisible(true);
+        }
+}
